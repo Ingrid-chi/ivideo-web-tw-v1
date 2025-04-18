38795b24a131eb91aa4b281340843f7f46510e0e
Ingrid <ingrid.chi77@gmail.com>
2025-04-18 11:37:51 +0800

    add git page need

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ivideo-web-tw-v1/',
+  build: {
+    outDir: 'docs', // ← 改成 docs 資料夾
+  },
   plugins: [react(), svgr()],
 });
